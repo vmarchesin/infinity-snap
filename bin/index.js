9b14dd86d3c902010968c670811f588f0759759a
Vinicius Marchesin Araujo <vinicius.marchesinaraujo@gmail.com>
2019-03-07 13:52:42 -0300

    v0.3
    Adds command-line-args to help parse options. Enables a verbose option and custom phrases at the end of execution.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const { array, spawn: { sh }, fileHelpers } = require('./utils');
 const os = require("os");
+const commandLineArgs = require('command-line-args')
 
 async function getFiles(path, preserveGit = false) {
   const { stdout } = await sh(path, preserveGit);
@@ -8,17 +9,20 @@ async function getFiles(path, preserveGit = false) {
   return array.shuffle(stdout);
 };
 
-function snap(filenames) {
+function snap(filenames, verbose) {
   for (let file of filenames) {
-    fileHelpers.deleteFile(file);
+    if (verbose) {
+      console.log(`Deleting file: ${file}`);
+    }
+    // fileHelpers.deleteFile(file);
   }
 };
 
-async function run({ path, preserveGit = false } = {}) {
+async function run({ path, preserveGit = false, verbose = false } = {}) {
   const filenames = await getFiles(path, preserveGit);
 
   filenames.splice(0, Math.floor(filenames.length / 2));
-  // snap(filenames);
+  snap(filenames, verbose);
   console.log(array.random([
     `Mr ${os.userInfo().username}, I don't feel so good...`,
     "Perfectly balanced, as all things should be.",
@@ -26,9 +30,13 @@ async function run({ path, preserveGit = false } = {}) {
   ]));
 }
 
-let path = process.cwd();
-if (process.argv.length > 2) {
-  path = process.argv[2];
-}
+const optionDefinitions = [
+  { name: 'verbose', alias: 'v', type: Boolean },
+  { name: 'preserve-git', alias: 'g', type: Boolean },
+  { name: 'path', type: String, multiple: false, defaultOption: true },
+];
 
-run({ path, preserveGit: true });
+const options = commandLineArgs(optionDefinitions);
+const path = options.path || process.cwd();
+
+run({ path, preserveGit: options['preserve-git'], verbose: options.verbose });
